f90bdf05bcd5c2540beede2ca9cddfdda78f4f38
Andrew Song <snappysong@gmail.com>
2021-11-08 10:03:07 -0800

    using textinputs

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Song');
+  const [age, setAge] = useState('30');
+
+  const clickHandler = () => {
+    if (name === 'Song'){
+      setName('Subzero')
+    }
+    if (name === 'Subzero'){
+      setName('Song')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>This is my<Text> test </Text> Text component</Text>
+        <Text>This is my Text component</Text>
+
+
+        <Text>This is my name, {name}.</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='update state' onPress={clickHandler} />
+        </View>
+
+        <Text>name: {name}, age: {age}</Text>
+        <Text>Enter name:</Text>
+        <TextInput
+          multiline
+          style={styles.input}
+          placeholder='e.g. John Doe'
+          onChangeText={(value) => setName(value)} />
+        <Text>Enter age:</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          placeholder='e.g. 99'
+          onChangeText={(value) => setAge(value)} />
+      </View>    
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +55,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }, 
+  body: {
+    backgroundColor: 'yellow',
+    padding: 30,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
